5cc9691c3b4d1fe695fe11310fd67cbe0df947d3
Matt Mohan <matt.mohan@wishabi.com>
2022-12-04 11:56:25 -0500

    Fix default task

--- runner.ts
+++ runner.ts
@@ -10,8 +10,9 @@ new Command()
   .name("Advent")
   .version("2022.2.0")
   .description("Solve Advent of Code 2022")
+  .default("help")
   .command("solve", Solve)
   //  .command("upgrade", Upgrade)
   .command("completions", new CompletionsCommand())
   .command("help", new HelpCommand().global())
-  .parse(Deno.args);
+  .parse();
