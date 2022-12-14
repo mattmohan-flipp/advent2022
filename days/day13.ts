738fd9cb89d1e731c1c9106a39a7ee32917d2805
Matt Mohan <matt.mohan@wishabi.com>
2022-12-13 21:52:41 -0500

    Cleanup day13

--- days/day13.ts
+++ days/day13.ts
@@ -1,49 +1,33 @@
+import { add } from "../helpers/array.ts";
+
 type NumberOrNumberArray = number | number[] | NumberOrNumberArray[];
 
 const parseBrackets = (input: string): NumberOrNumberArray => JSON.parse(input);
 
-// Values manually set to hack the sort function
-enum State {
-  VALID = -1,
-  INVALID = 1,
-  UNKNOWN = 0,
-}
-
 const compare = (
   left: NumberOrNumberArray,
   right: NumberOrNumberArray
-): State => {
+): number => {
   // Compare numbers first
   if (!Array.isArray(left) && !Array.isArray(right)) {
-    if (left < right) {
-      return State.VALID;
-    } else if (left > right) {
-      return State.INVALID;
-    }
-    return State.UNKNOWN;
+    return left - right;
   }
+
   // If either side isn't an array convert and compare
   if (!Array.isArray(left)) {
     return compare([left], right);
-  }
-  if (!Array.isArray(right)) {
+  } else if (!Array.isArray(right)) {
     return compare(left, [right]);
   }
 
   // Otherwise compare the array elements
   for (let i = 0; i < left.length && i < right.length; i++) {
     const res = compare(left[i], right[i]);
-    if (res !== State.UNKNOWN) {
+    if (res !== 0) {
       return res;
     }
   }
-  if (left.length > right.length) {
-    return State.INVALID;
-  }
-  if (left.length < right.length) {
-    return State.VALID;
-  }
-  return State.UNKNOWN;
+  return left.length - right.length;
 };
 
 // Check if the input conforms to [[num]]
@@ -63,8 +47,8 @@ const day = {
       .split("\n\n") // Split into packets
       .map((pair) => pair.split("\n").map(parseBrackets)) //Parse into a tuple of [left,right]
       .map(([left, right]) => compare(left, right)) // Compare each pair
-      .map((e, idx) => (e === State.VALID ? idx + 1 : 0)) // Adjust for 1-indexed output and skip invalid
-      .reduce((a, c) => a + c, 0)
+      .map((e, idx) => (e <= -1 ? idx + 1 : 0)) // Adjust for 1-indexed output and skip invalid
+      .reduce(add, 0)
       .toString(),
   b: (file: string): string => {
     const packets = file
--- helpers/array.ts
+++ helpers/array.ts
@@ -7,8 +7,18 @@ export const teeLog = <T>(header: string, transformer: (arg0: T) => string) => {
     headerRow[0].repeat(headerRow.length),
   ].join("\n");
   console.log(headerText);
-  return (e: T, idx: number) => {
+  return tee((e: T, idx: number) => {
     console.log(`${idx} => ${transformer(e)}`);
+  });
+};
+
+export const tee =
+  <T>(callback: (e: T, idx: number) => void) =>
+  (e: T, idx: number) => {
+    callback(e, idx);
     return e;
   };
-};
+
+export const add = (a: number, b: number) => a + b;
+export const clamp = (min: number, max: number) => (a: number) =>
+  Math.min(Math.max(a, min), max);
